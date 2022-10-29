5332d6b0af7939e5460bbf980500cabbabafde3a
gyulabodor <gyulabodor95@gmail.com>
2022-10-29 18:00:26 +0200

    Created fetchTMDBDiscoverMovies() request for related search which will response back a list of movies by the given actorID

--- src/requests/fetchTMDB.js
+++ src/requests/fetchTMDB.js
@@ -15,7 +15,7 @@ export const fetchTMDBSearchMovie = async (title)  => {
                     url: custom(size: "w185_and_h278_bestv2")
                 }
             }
-        }`
+        }`;
    const response = await fetch(tmdb_url,  {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
@@ -26,6 +26,37 @@ export const fetchTMDBSearchMovie = async (title)  => {
 		throw new Error(`HTTP error status: ${response.status}`);
 	}
 
-    const data = await response.json()
+    const data = await response.json();
     return data;
 }
+
+
+export const fetchTMDBDiscoverMovies = async (actorID) => {
+    const query = `
+        query DiscoverMovies {
+            discoverMovies(filter: { withCast: { include: [${actorID}] } }) {
+                id
+                name
+                releaseDate
+                score
+                img: poster {
+                    url: custom(size: "w185_and_h278_bestv2")
+                    }
+                }
+        }`;
+
+    const response = await fetch(tmdb_url,  {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({query: query}),
+    })
+
+    if (!response.ok) {
+		throw new Error(`HTTP error status: ${response.status}`);
+	}
+
+    const data = await response.json();
+    return data;
+}
+
+
