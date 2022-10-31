935b22715ef5aa875e763e62f6b8a0cbfc2d3260
gyulabodor <gyulabodor95@gmail.com>
2022-10-31 17:15:42 +0100

    Added some styling for App component and mmade a new theme

--- src/App.js
+++ src/App.js
@@ -1,35 +1,32 @@
-import { Typography } from '@mui/material';
-import { useEffect, useState } from 'react';
-import './App.css';
+import { Stack, ThemeProvider, Typography } from '@mui/material';
+import { useState } from 'react';
 import MovieList from './components/MovieList/MovieList';
 import { SearchForm } from './components/SearchField/SearchForm';
 import { LoadingContext, MovieListContext } from './utilities/Context';
 import { Spinner } from "./components/Spinner/Spinner";
-
+import { appStackStyle } from './appStyle';
+import { theme } from './appTheme'
 
 export default function App() {
   const [movies,setMovies] = useState([]) 
   const [loading, setLoading] = useState(false); 
   
-  useEffect(() => {
-    document.title = "Movie Finder";
-  })
-
-  const handleSearchClick = (movies) => {
-    setMovies(movies);
-  }
-
   return (
-    <LoadingContext.Provider value={{loading,setLoading}}>
-      <MovieListContext.Provider value={{movies,setMovies}}>
-        <div className="App">
-          <Typography variant='h4'>Movie Finder</Typography>
-          <SearchForm handleClick={handleSearchClick}/>
-          <MovieList movies={movies}/>
-          { loading ? <Spinner isLoading={loading}/> : ""}
-        </div>
-      </MovieListContext.Provider>
-    </LoadingContext.Provider>
+    <ThemeProvider theme={theme}>
+      <LoadingContext.Provider value={{loading,setLoading}}>
+        <MovieListContext.Provider value={{movies,setMovies}}>
+          <Stack
+            direction="column"
+            sx={appStackStyle}
+          >
+            <Typography variant='h4'>Movie Finder</Typography>
+            <SearchForm/>
+            <MovieList movies={movies}/>
+            { loading ? <Spinner isLoading={loading}/> : ""}
+          </Stack>
+        </MovieListContext.Provider>
+      </LoadingContext.Provider>
+    </ThemeProvider>
   );
 }
 
---
+++ src/appStyle.js
@@ -0,0 +1,10 @@
+export const appStackStyle = {
+    width: '90%',
+    margin: '10px auto',
+    alignItems: 'center',
+    padding: '10px',
+    background: '#fff5f0',
+    borderRadius: '10px',
+    boxShadow:'0px 3px 3px 3px rgba(0,0,0,.2)',
+    fontFamily: `'Roboto Slab', 'serif'`
+}
