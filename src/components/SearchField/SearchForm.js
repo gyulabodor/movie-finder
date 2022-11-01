2a0972a99bc39de9a555056aa85be7bfd0a96c5e
gyulabodor <gyulabodor95@gmail.com>
2022-11-01 18:48:20 +0100

    Added onKeyPress event when hit ENTER the search starts

--- src/components/SearchField/SearchForm.js
+++ src/components/SearchField/SearchForm.js
@@ -1,50 +1,68 @@
-import { Button, TextField } from "@mui/material"
-import { useContext, useState } from "react"
-import LocationSearchingOutlinedIcon from '@mui/icons-material/LocationSearchingOutlined';
+import { Button, TextField } from "@mui/material";
+import { useContext, useState } from "react";
+import LocationSearchingOutlinedIcon from "@mui/icons-material/LocationSearchingOutlined";
 import { Stack } from "@mui/system";
 import { fetchTMDBSearchMovie } from "../../requests/fetchTMDB";
-import {  MovieListContext, InfoContext, LoadingContext } from "../../utilities/Context";
-
+import {
+  MovieListContext,
+  InfoContext,
+  LoadingContext,
+} from "../../utilities/Context";
 
 export const SearchForm = () => {
-    
-    const [searchTitle,setSearchTitle] = useState("");
-    const {setLoading} = useContext(LoadingContext)
-    const {setMovies} = useContext(MovieListContext);
-    const {setIsRelated,setSearched} = useContext(InfoContext);
+  const [searchTitle, setSearchTitle] = useState("");
+  const { setLoading } = useContext(LoadingContext);
+  const { setMovies } = useContext(MovieListContext);
+  const { setIsRelated, setSearched } = useContext(InfoContext);
 
-    const submitSearch = async () => {
-        if (searchTitle !== "") {
-            setLoading(true);
-            const resultArray = await fetchTMDBSearchMovie(searchTitle);
-            setMovies(resultArray.data.searchMovies);
-            setLoading(false);
-            setSearchTitle("");
-            setIsRelated(false);
-            setSearched(true);
-        }
+  const handleKeyPress = (e) => {
+    if (e.key === "Enter") {
+      submitSearch();
     }
+  }
 
-    return(
-        <Stack spacing={2} direction="row">
-            <TextField 
-                label="Movie title" 
-                variant="outlined" 
-                size="large"
-                color="error"
-                type="text"
-                value={searchTitle}
-                onChange={e => setSearchTitle(e.target.value)}
-            />
-                
-            <Button
-                onClick={e => submitSearch()} 
-                variant="contained" 
-                size="large"
-                color="error"
-                startIcon={<LocationSearchingOutlinedIcon/>}
-            >Search
-            </Button>
-        </Stack>
-    )
-}
+  const submitSearch = async () => {
+    if (searchTitle !== "") {
+      setLoading(true);
+      const resultArray = await fetchTMDBSearchMovie(searchTitle);
+      setMovies(resultArray.data.searchMovies);
+      setSearchTitle("");
+      setIsRelated(false);
+      setSearched(true);
+      setLoading(false);
+    }
+    else{
+      setLoading(true);
+      setMovies([]);
+      setSearchTitle("");
+      setIsRelated(false);
+      setSearched(false);
+      setLoading(false);
+    }
+  };
+
+  return (
+    <Stack spacing={2} direction="row">
+      <TextField
+        label="Movie title"
+        variant="outlined"
+        size="large"
+        color="error"
+        type="text"
+        value={searchTitle}
+        onChange={(e) => setSearchTitle(e.target.value)}
+        onKeyPress={(e) => handleKeyPress(e)}
+      />
+
+      <Button
+        onClick={(e) => submitSearch()}
+        variant="contained"
+        size="large"
+        color="error"
+        startIcon={<LocationSearchingOutlinedIcon />}
+      >
+        Search
+      </Button>
+    </Stack>
+  );
+};
